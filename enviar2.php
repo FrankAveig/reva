495f1a2038d18f3cb8d3e69dc6209eb49feb90a2
Frank Aveiga <xfpad93@gmail.com>
2020-10-23 02:02:26 -0500

    pagina completa dd reva

---
+++ enviar2.php
@@ -0,0 +1,59 @@
+<?php
+//Recipiente
+$to = 'fpad_93@hotmail.com';
+
+//remitente del correo
+$from = 'remitente@miexample.com';
+$fromName = 'BaulPHP';
+
+//Asunto del email
+$subject = 'Correo enviado desde la pagina web'; 
+
+//Ruta del archivo adjunto
+$file = "mipdf.pdf";
+
+//Contenido del Email
+$message = "Este mensaje fue enviado por: " . $name . " \r\n";
+$message .= "Su e-mail es: " . $mail . " \r\n";
+$message .= "Teléfono de contacto: " . $phone . " \r\n";
+$message .= "Tipo de voluntariado: " . $menu . " \r\n";
+$message .= "Mensaje: " . $_POST['message'] . " \r\n";
+$message .= "Enviado el: " . date('d/m/Y', time());
+
+//Encabezado para información del remitente
+$headers = "De: $fromName"." <".$from.">";
+
+//Limite Email
+$semi_rand = md5(time()); 
+$mime_boundary = "==Multipart_Boundary_x{$semi_rand}x"; 
+
+//Encabezados para archivo adjunto 
+$headers .= "\nMIME-Version: 1.0\n" . "Content-Type: multipart/mixed;\n" . " boundary=\"{$mime_boundary}\""; 
+
+//límite multiparte
+$message = "--{$mime_boundary}\n" . "Content-Type: text/html; charset=\"UTF-8\"\n" .
+"Content-Transfer-Encoding: 7bit\n\n" . $htmlContent . "\n\n"; 
+
+//preparación de archivo
+if(!empty($file) > 0){
+    if(is_file($file)){
+        $message .= "--{$mime_boundary}\n";
+        $fp =    @fopen($file,"rb");
+        $data =  @fread($fp,filesize($file));
+
+        @fclose($fp);
+        $data = chunk_split(base64_encode($data));
+        $message .= "Content-Type: application/octet-stream; name=\"".basename($file)."\"\n" . 
+        "Content-Description: ".basename($files[$i])."\n" .
+        "Content-Disposition: attachment;\n" . " filename=\"".basename($file)."\"; size=".filesize($file).";\n" . 
+        "Content-Transfer-Encoding: base64\n\n" . $data . "\n\n";
+    }
+}
+$message .= "--{$mime_boundary}--";
+$returnpath = "-f" . $from;
+
+//Enviar EMail
+$mail = @mail($to, $subject, $message, $headers, $returnpath); 
+
+//Estado de envío de correo electrónico
+echo $mail?"<h1>Correo enviado.</h1>":"<h1>El envío de correo falló.</h1>";
